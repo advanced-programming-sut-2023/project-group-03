c778db3273a1012d985e90c0e68938cbc7eabd94
ali <alitwis83@gmail.com>
2023-05-12 22:20:14 +0330

    quick fix

--- projectAP-fixed/Stronghold/src/main/java/Model/Buildings/Enums/ResourceTypes.java
+++ projectAP-fixed/Stronghold/src/main/java/Model/Buildings/Enums/ResourceTypes.java
@@ -3,10 +3,20 @@ package Model.Buildings.Enums;
 import java.util.ArrayList;
 
 public enum ResourceTypes {
-    WEAPON,
-    FOOD,
-    STOCK,
+    WEAPON("weapon"),
+    FOOD("food"),
+    STOCK("stock"),
     ;
+
+    private String name;
+
+    ResourceTypes(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     private ArrayList<Resources> subset = new ArrayList<>();
     private InventoryTypes inventoryType;
