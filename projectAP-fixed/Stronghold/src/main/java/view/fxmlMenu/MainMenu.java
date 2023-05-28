2ef0081b1f5264b46916ad27a3db94dd84c22f9f
MT82 <119634469+MehranTalaei@users.noreply.github.com>
2023-05-28 22:29:07 +0430

    done

---
+++ projectAP-fixed/Stronghold/src/main/java/view/fxmlMenu/MainMenu.java
@@ -0,0 +1,20 @@
+package view.fxmlMenu;
+
+import javafx.event.ActionEvent;
+
+public class MainMenu {
+    public void start(ActionEvent actionEvent) {
+    }
+
+    public void map(ActionEvent actionEvent) {
+    }
+
+    public void profile(ActionEvent actionEvent) {
+    }
+
+    public void scoreboard(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+}
