da3c5ca85e88b03ba9d3ef6c31b7ebcf1658ab0e
MT82 <119634469+MehranTalaei@users.noreply.github.com>
2023-05-29 01:34:21 +0430

    salam

--- projectAP-fixed/Stronghold/src/main/java/view/Controllers/StartingController.java
+++ projectAP-fixed/Stronghold/src/main/java/view/Controllers/StartingController.java
@@ -1,14 +1,23 @@
 package view.Controllers;
 
+import Model.Defaults;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import view.fxmlMenu.EndingMenu;
 
-public class StartingController {
+public class StartingController extends Application {
     public void exit(ActionEvent actionEvent) {
+        new EndingMenu().start();
     }
 
     public void login(ActionEvent actionEvent) {
     }
 
     public void register(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
     }
 }
---
+++ projectAP-fixed/Stronghold/src/main/java/view/fxmlMenu/EndingMenu.java
@@ -0,0 +1,11 @@
+package view.fxmlMenu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EndingMenu extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.close();
+    }
+}
