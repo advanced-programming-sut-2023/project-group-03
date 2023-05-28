18efc66e8fdfcbd8223313291debd1847f986f2f
ali <alitwis83@gmail.com>
2023-05-28 18:44:57 +0330

    needed default

--- projectAP-fixed/Stronghold/src/main/java/view/Controllers/ProfileMenuFXController.java
+++ projectAP-fixed/Stronghold/src/main/java/view/Controllers/ProfileMenuFXController.java
@@ -1,5 +1,37 @@
 package view.Controllers;
 
-public class ProfileMenuFXController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileMenuFXController implements Initializable {
+
+
+    public HBox profile;
+
+    public void changeUsername(ActionEvent actionEvent) {
+    }
+
+    public void changePassword(ActionEvent actionEvent) {
+    }
+
+    public void changeEmail(ActionEvent actionEvent) {
+    }
+
+    public void changeAvatar(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void changeSlogan(ActionEvent actionEvent) {
+    }
+
+    public void clearSlogan(ActionEvent actionEvent) {
+    }
 }
