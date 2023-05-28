02495690ca48788e9f1c5810c5e01ffc4233a494
MT82 <119634469+MehranTalaei@users.noreply.github.com>
2023-05-28 19:20:31 +0430

    salam

--- projectAP-fixed/Stronghold/src/main/java/view/Controllers/RegisterController.java
+++ projectAP-fixed/Stronghold/src/main/java/view/Controllers/RegisterController.java
@@ -1,4 +1,23 @@
 package view.Controllers;
 
+import javafx.event.ActionEvent;
+
 public class RegisterController {
+    public void back(ActionEvent actionEvent) {
+    }
+
+    public void next(ActionEvent actionEvent) {
+    }
+
+    public void reset(ActionEvent actionEvent) {
+    }
+
+    public void resetConfirm(ActionEvent actionEvent) {
+    }
+
+    public void backConfirm(ActionEvent actionEvent) {
+    }
+
+    public void nextCofirm(ActionEvent actionEvent) {
+    }
 }
