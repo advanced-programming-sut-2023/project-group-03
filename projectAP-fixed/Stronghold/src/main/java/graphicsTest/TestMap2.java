c831d13ddc341da8d5c3e2cc73fc50eba32f9e65
ali <alitwis83@gmail.com>
2023-06-07 02:01:58 +0330

    fixed map movement

--- projectAP-fixed/Stronghold/src/main/java/graphicsTest/TestMap2.java
+++ projectAP-fixed/Stronghold/src/main/java/graphicsTest/TestMap2.java
@@ -10,16 +10,17 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class TestMap2 extends Application {
-    int tileSize = 60;
+    int tileSize = 50;
     int tileSizeDelta = 20;
-    int satr = 100;
-    int sotoon = 100;
+    int satr = 400;
+    int sotoon = 400;
     int halfSatr = satr / 2;
     int halfSotoon = sotoon / 2;
     double iDivider = 2;
@@ -34,6 +35,7 @@ public class TestMap2 extends Application {
 
     HashSet<Polygon> currentTiles = new HashSet<>();
     Polygon[][] allRecs = new Polygon[satr][sotoon];
+    Polygon view;
     Pane mapPane = new Pane();
 
     public Polygon getHex(int height, int i, int j, int iSize, int jSize, String image, Pane pane) {
@@ -100,23 +102,35 @@ public class TestMap2 extends Application {
         Scene gameScene = new Scene(pane);
         stage.setScene(gameScene);
 
+        int halfSums = - halfSatr - halfSotoon;
+        int halfMinus = - halfSatr + halfSotoon;
         //mehran's rectangle
-        Polygon view = new Polygon(
-                ((double)(halfSatr)) / iDivider * tileSize, ((double)(halfSatr)) / jDivider * tileSize,
-                ((double)(satr - halfSotoon)) / iDivider * tileSize, ((double)(satr + halfSotoon)) / jDivider * tileSize,
-                ((double)(halfSatr - sotoon)) / iDivider * tileSize, ((double)(halfSatr + sotoon)) / jDivider * tileSize,
-                ((double)( - halfSotoon)) / iDivider * tileSize, ((double)(halfSotoon)) / jDivider * tileSize
+//        view = new Polygon(
+//                ((double)(halfSatr + halfMinus)) / iDivider * tileSize, ((double)(halfSatr + halfSums)) / jDivider * tileSize,
+//                ((double)(satr - halfSotoon + halfMinus)) / iDivider * tileSize, ((double)(satr + halfSotoon + halfSums)) / jDivider * tileSize,
+//                ((double)(halfSatr - sotoon + halfMinus)) / iDivider * tileSize, ((double)(halfSatr + sotoon + halfSums)) / jDivider * tileSize,
+//                ((double)( - halfSotoon + halfMinus)) / iDivider * tileSize, ((double)(halfSotoon + halfSums)) / jDivider * tileSize
+//        );
+
+        view = new Polygon(
+                -stage.getWidth(), 0f,
+                stage.getWidth(), 0f,
+                stage.getWidth(), stage.getHeight(),
+                -stage.getWidth(), stage.getHeight()
         );
+
         view.setFill(Color.TRANSPARENT);
-        mapPane.getChildren().add(view);
+//        mapPane.getChildren().add(view);
+        view.setLayoutY(0);
+        view.setLayoutX(0);
 
         double x1 = view.getPoints().get(0);
         double y1 = view.getPoints().get(1);
         mapPane.setLayoutX(x1);
         mapPane.setLayoutY(-y1);
         mapPane.setLayoutX(stage.getWidth() / 2);
-        mapPane.setLayoutY(-10 * verticalCameraMove);
-
+//        mapPane.setLayoutY(20 * verticalCameraMove);
+        mapPane.setLayoutY(0);
         stage.show();
 
         gameScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
@@ -129,9 +143,12 @@ public class TestMap2 extends Application {
         gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
+                currentTile[0] = 50;
+                currentTile[1] = 50;
                 if (keyEvent.getCode().equals(KeyCode.LEFT)) {
                     if (currentTile[0] > 0 && currentTile[1] < sotoon - cameraCol / 2) {
                         mapPane.setLayoutX(mapPane.getLayoutX() + horizontalCameraMove);
+                        view.setLayoutX(view.getLayoutX() - horizontalCameraMove);
                         currentTile[1]++;
                         currentTile[0]--;
                         updateCamera();
@@ -139,15 +156,15 @@ public class TestMap2 extends Application {
                 } else if (keyEvent.getCode().equals(KeyCode.RIGHT)) {
                     if (currentTile[0] < satr - cameraRow / 2 && currentTile[1] > 0) {
                         mapPane.setLayoutX(mapPane.getLayoutX() - horizontalCameraMove);
+                        view.setLayoutX(view.getLayoutX() + horizontalCameraMove);
                         currentTile[1]--;
                         currentTile[0]++;
                         updateCamera();
                     }
                 } else if (keyEvent.getCode().equals(KeyCode.UP)) {
-                    if (currentTile[0] > -20 && currentTile[1] > -20) {
-                        mapPane.setLayoutY(mapPane.getLayoutY() + verticalCameraMove);
-                    }
                     if (currentTile[0] > 0 && currentTile[1] > 0) {
+                        mapPane.setLayoutY(mapPane.getLayoutY() + verticalCameraMove);
+                        view.setLayoutY(view.getLayoutY() - verticalCameraMove);
                         currentTile[1]--;
                         currentTile[0]--;
                         updateCamera();
@@ -155,6 +172,7 @@ public class TestMap2 extends Application {
                 } else if (keyEvent.getCode().equals(KeyCode.DOWN)) {
                     if (currentTile[0] < satr - cameraRow / 2 && currentTile[1] < sotoon - cameraCol / 2) {
                         mapPane.setLayoutY(mapPane.getLayoutY() - verticalCameraMove);
+                        view.setLayoutY(view.getLayoutY() + verticalCameraMove);
                         currentTile[1]++;
                         currentTile[0]++;
                         updateCamera();
@@ -175,14 +193,23 @@ public class TestMap2 extends Application {
             mapPane.getChildren().remove(polygon);
         }
         currentTiles.clear();
-        for (int x = currentTile[0]; x < currentTile[0] + cameraRow; x++) {
-            for (int y = currentTile[1]; y < currentTile[1] + cameraCol; y++) {
-                if (x >= satr) continue;
-                if (y >= sotoon) continue;
-                currentTiles.add(allRecs[x][y]);
-                mapPane.getChildren().add(allRecs[x][y]);
+        for (int x = 0; x < satr; x++) {
+            for (int y = 0; y < sotoon; y++) {
+                Polygon polygon = allRecs[x][y];
+                if (polygon.getBoundsInParent().intersects(view.getBoundsInParent())) {
+                    mapPane.getChildren().add(polygon);
+                    currentTiles.add(polygon);
+                }
             }
         }
+//        for (int x = currentTile[0]; x < currentTile[0] + cameraRow; x++) {
+//            for (int y = currentTile[1]; y < currentTile[1] + cameraCol; y++) {
+//                if (x >= satr) continue;
+//                if (y >= sotoon) continue;
+//                currentTiles.add(allRecs[x][y]);
+//                mapPane.getChildren().add(allRecs[x][y]);
+//            }
+//        }
     }
 
     private void updateAllRecs() {
@@ -206,15 +233,15 @@ public class TestMap2 extends Application {
     }
 
     private void changeZoom(boolean plus) {
-        if (plus && tileSize == 100) return;
-        if (!plus && tileSize == 20) return;
+        if (plus && tileSize >= 100) return;
+        if (!plus && tileSize <= 20) return;
         if (plus) tileSize += tileSizeDelta;
         else tileSize -= tileSizeDelta;
 //        for (Polygon polygon : currentTiles) {
 //            mapPane.getChildren().remove(polygon);
 //        }
         mapPane.getChildren().clear();
-        currentTiles.clear();
+//        currentTiles.clear();
         allRecs = new Polygon[satr][sotoon];
         updateAllRecs();
         verticalCameraMove = 2 / jDivider * tileSize;
