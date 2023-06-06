e36ab98def57d475315ae798dd7ffae8d5209b05
MT82 <119634469+MehranTalaei@users.noreply.github.com>
2023-06-06 15:54:17 +0430

    saved

---
+++ projectAP-fixed/Stronghold/src/main/java/graphicsTest/TestMap3.java
@@ -0,0 +1,11 @@
+package graphicsTest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestMap3 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
