3a724a55101113f6f3b9c346de04a186c711985a
MT82 <119634469+MehranTalaei@users.noreply.github.com>
2023-06-30 04:38:59 -0700

    salam

--- projectAP-fixed/Stronghold/src/main/java/view/Network/LaunchInitializer.java
+++ projectAP-fixed/Stronghold/src/main/java/view/Network/LaunchInitializer.java
@@ -2,18 +2,26 @@ package view.Network;
 
 import Model.GamePlay.Game;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import view.fxmlMenu.GameLayout;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
-public class LaunchInitializer {
+public class LaunchInitializer implements Initializable{
     static Pane fxmlPane;
 
     static {
@@ -24,5 +32,21 @@ public class LaunchInitializer {
         }
     }
 
-    
+    public VBox ServerVbox;
+    public Label ServerAddress;
+    public Button exit;
+    public Button next;
+    public VBox MapVbox;
+    public Label ServerAddress1;
+    public TextField mapNumber;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ArrayList strings = new ArrayList<String>();
+        strings.add("salam");
+        strings.add("bye");
+        strings.add("vai");
+        listOfPlayers.setItems(FXCollections.observableList(strings));
+    }
 }
